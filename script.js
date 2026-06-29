92008573b851d426834d8038ba3c6d38d3816820
chandranaibaho61-hash <chandranaibaho61@gmail.com>
2026-06-29 22:34:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function openLove(){
+
+    const message = document.getElementById("love");
+
+
+    if(message.style.display === "block"){
+
+        message.style.display="none";
+
+    }
+
+    else{
+
+        message.style.display="block";
+
+    }
+
+
+}
